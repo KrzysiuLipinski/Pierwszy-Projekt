8ef5d980a50cf88a4bccbded2f169d74d754a771
KrzysiuLipinski <krislipa70@gmail.com>
2023-10-24 17:34:04 +0200

    Zadanie dzien 4

--- Pierwszy Projekt/Pierwszy Projekt/Program.cs
+++ Pierwszy Projekt/Pierwszy Projekt/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 33;
+var sex = "kobieta";
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "kobieta" && age < 33)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age < 18 && sex != "kobieta")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Złe dane");
+}
